b279e07a238083c849159b7c9e6f51a93b37fb51
Michelle Blumenstein <floreum.saxum@gmail.com>
2016-10-21 17:47:25 +0200

    LabView DLL finished

--- LabView_dll/LabView_dll/Class1.cs
+++ LabView_dll/LabView_dll/Class1.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace LabView_dll
 {
-    public static class LabView
+    public class LabView
     {
-        public static double verdopple(double x)
+        public double verdopple(double x)
         { return 2 * x; }
     }
 }
