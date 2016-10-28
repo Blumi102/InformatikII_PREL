e6b7e8b67c89f1d0c49c585626d7d89e19776752
Michelle Blumenstein <floreum.saxum@gmail.com>
2016-10-28 18:04:27 +0200

    ListT Stopwatch

--- A02_Diagram_Chart_ListT_Stopwatch/WahlaufgabenI_A02/Form1.Designer.cs
+++ A02_Diagram_Chart_ListT_Stopwatch/WahlaufgabenI_A02/Form1.Designer.cs
@@ -28,20 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.Start_button = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.reset_button = new System.Windows.Forms.Button();
+            this.ergebnis_chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.durchlaufe_label = new System.Windows.Forms.Label();
             this.Zeit_label = new System.Windows.Forms.Label();
             this.aussen_label = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ergebnis_chart)).BeginInit();
             this.SuspendLayout();
             // 
             // Start_button
@@ -55,39 +54,30 @@
             this.Start_button.UseVisualStyleBackColor = false;
             this.Start_button.Click += new System.EventHandler(this.Start_button_Click);
             // 
-            // button2
+            // reset_button
             // 
-            this.button2.Location = new System.Drawing.Point(130, 31);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Auswertung";
-            this.button2.UseVisualStyleBackColor = true;
+            this.reset_button.Location = new System.Drawing.Point(130, 32);
+            this.reset_button.Name = "reset_button";
+            this.reset_button.Size = new System.Drawing.Size(75, 23);
+            this.reset_button.TabIndex = 2;
+            this.reset_button.Text = "Reset";
+            this.reset_button.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // ergebnis_chart
             // 
-            this.button3.Location = new System.Drawing.Point(211, 32);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Reset";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // chart1
-            // 
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
-            this.chart1.Location = new System.Drawing.Point(130, 80);
-            this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart1.Series.Add(series2);
-            this.chart1.Size = new System.Drawing.Size(300, 300);
-            this.chart1.TabIndex = 3;
-            this.chart1.Text = "chart1";
+            chartArea1.Name = "ChartArea1";
+            this.ergebnis_chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.ergebnis_chart.Legends.Add(legend1);
+            this.ergebnis_chart.Location = new System.Drawing.Point(130, 80);
+            this.ergebnis_chart.Name = "ergebnis_chart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.ergebnis_chart.Series.Add(series1);
+            this.ergebnis_chart.Size = new System.Drawing.Size(580, 300);
+            this.ergebnis_chart.TabIndex = 3;
+            this.ergebnis_chart.Text = "chart1";
             // 
             // label1
             // 
@@ -147,20 +137,19 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(463, 424);
+            this.ClientSize = new System.Drawing.Size(748, 424);
             this.Controls.Add(this.aussen_label);
             this.Controls.Add(this.Zeit_label);
             this.Controls.Add(this.durchlaufe_label);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.chart1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ergebnis_chart);
+            this.Controls.Add(this.reset_button);
             this.Controls.Add(this.Start_button);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ergebnis_chart)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -169,9 +158,8 @@
         #endregion
 
         private System.Windows.Forms.Button Start_button;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button reset_button;
+        private System.Windows.Forms.DataVisualization.Charting.Chart ergebnis_chart;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label duchlaufe_label;
         private System.Windows.Forms.Label label2;
